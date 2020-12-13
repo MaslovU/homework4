cb0c983883397b0006e3edcae282032d4f6b22e9
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2020-12-13 17:37:36 +0500

    homework4 maven

---
+++ src/main/java/com/maslov/App.java
@@ -0,0 +1,13 @@
+package com.maslov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
